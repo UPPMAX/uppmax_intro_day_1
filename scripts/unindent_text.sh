8eb941f4cdb998be0a3a912044680e2fc5eba8fd
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-06-12 14:42:47 +0200

    Fix script style

--- scripts/unindent_text.sh
+++ scripts/unindent_text.sh
@@ -7,9 +7,10 @@
 # Here all indented text is unindented
 # https://github.com/UPPMAX/UPPMAX-documentation/issues/114
 #
+
 for filename in $(find . | grep .md)
 do
-sed -i 's/^    //g' "${filename}"
-sed -i 's/^    //g' "${filename}"
-sed -i 's/^    //g' "${filename}"
+    sed -i 's/^    //g' "${filename}"
+    sed -i 's/^    //g' "${filename}"
+    sed -i 's/^    //g' "${filename}"
 done
